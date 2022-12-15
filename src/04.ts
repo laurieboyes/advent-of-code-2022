250c76fb46988fd9a542dc71de156a2bb493f60b
Laurie Boyes <laurie.boyes@beamery.com>
2022-12-15 23:00:39 +0000

    My score’s going down but I’m happier

--- src/04.ts
+++ src/04.ts
@@ -1,3 +1,5 @@
+//https://adventjs.dev/challenges/2022/4
+
 function fitsInOneBox(boxes) {
   if (boxes.length <= 1) {
     return true;
@@ -9,27 +11,23 @@ function fitsInOneBox(boxes) {
     h: number;
   };
 
-  const getBoxNumber = (box: Box) => parseInt(`${box.l}${box.w}${box.h}`);
-
-  const sortedBoxes = boxes.sort((a, b) => getBoxNumber(a) - getBoxNumber(b));
-
   const boxFitsInBox = (innerBox: Box, outerBox: Box): boolean => {
-    const dimensionFit = (dimensionKey: 'l' | 'w' | 'h') => {
-      return innerBox[dimensionKey] < outerBox[dimensionKey];
-    };
+    const dimensionFit = (dimensionKey: 'l' | 'w' | 'h') =>
+      innerBox[dimensionKey] < outerBox[dimensionKey];
     return dimensionFit('l') && dimensionFit('w') && dimensionFit('h');
   };
 
-  const boxFitsInBoxes = (box: Box, remainingBoxes: Box[]) => {
-    return remainingBoxes.reduce((acc, otherBox) => {
-      return acc && boxFitsInBox(box, otherBox);
-    }, true);
-  };
+  const boxFitsInBoxes = (box: Box, remainingBoxes: Box[]) =>
+    remainingBoxes.every((otherBox) => boxFitsInBox(box, otherBox));
 
-  return sortedBoxes.reduce((acc, box, i) => {
-    const biggerBoxes = sortedBoxes.slice(i + 1);
-    return acc && boxFitsInBoxes(box, biggerBoxes);
-  }, true);
+  const getBoxSortKey = (box: Box) => parseInt(`${box.l}${box.w}${box.h}`);
+  const sortedBoxes = boxes.sort((a, b) => getBoxSortKey(a) - getBoxSortKey(b));
+
+  const elementsRemainingAfter = (arr, i) => arr.slice(i + 1);
+
+  return sortedBoxes.every((box, i) =>
+    boxFitsInBoxes(box, elementsRemainingAfter(sortedBoxes, i))
+  );
 }
 
 export default fitsInOneBox;
